ceba02a58b3ed4fbcfec8b2fcf976e7bc331b5de
Brian <Brian@Brians-MacBook-Air.local>
2019-10-29 22:47:31 -0500

    migrations table added and adjusted along with models

--- db/migrations.sql
+++ db/migrations.sql
@@ -8,15 +8,30 @@ CREATE TABLE users(
   username VARCHAR(32),
   password_digest VARCHAR(60)
 );
-
+CREATE TABLE tasks(
+  id SERIAL PRIMARY KEY,
+  task VARCHAR(255)
+);
+CREATE TABLE foods(
+  id SERIAL PRIMARY KEY,
+  food VARCHAR(255)
+);
+CREATE TABLE workouts(
+  id SERIAL PRIMARY KEY,
+  workout VARCHAR(255)
+);
+CREATE TABLE calories(
+  id SERIAL PRIMARY KEY,
+  calorie  NUMERIC NOT NULL DEFAULT 'NaN'
+);
 CREATE TABLE days(
   id SERIAL PRIMARY KEY,
   name VARCHAR(255),
-  time_awake VARCHAR(255),
-  task VARCHAR(255),
-  food VARCHAR(255),
-  workout VARCHAR(255),
-  calorie NUMERIC NOT NULL DEFAULT 'NaN',
+  time_awake TIME,
+  task_id INTEGER REFERENCES tasks(id),
+  food_id INTEGER REFERENCES foods(id),
+  workout_id INTEGER REFERENCES workouts(id),
+  calorie_id INTEGER REFERENCES calories(id),
   user_id INTEGER REFERENCES users(id)
 
 );
