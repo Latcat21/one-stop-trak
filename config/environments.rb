6180f3ead6f776abb9629b08ad1af9c514c52e7d
Brian <Brian@Brians-MacBook-Air.local>
2019-12-17 23:55:43 -0600

    heroku devolpement and production

--- config/environments.rb
+++ config/environments.rb
@@ -1,6 +1,6 @@
-configure :production, :development do
+configure :production do
 	
-	db = URI.parse(ENV['DATABASE_URL'] || 'postgres://localhost/one_stop')
+	db = URI.parse(ENV['DATABASE_URL'] )
 
 	ActiveRecord::Base.establish_connection(
 		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
@@ -10,4 +10,18 @@ configure :production, :development do
 		:database => db.path[1..-1],
 		:encoding => 'utf8'
 	)
+end
+
+configure :development do
+  db = URI.parse( 'postgres://localhost/one_stop')
+
+	ActiveRecord::Base.establish_connection(
+		:adapter => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		:host => db.host,
+		:username => db.user,
+		:password => db.password,
+		:database => db.path[1..-1],
+		:encoding => 'utf8'
+	)
+
 end
