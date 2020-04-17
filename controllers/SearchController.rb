7e91feed071e70c69dc2b1a6e8e8227ee1f579de
Brian <Brian@Brians-MacBook-Air.local>
2020-04-17 14:45:59 -0500

    meald api retreiving data

--- controllers/SearchController.rb
+++ controllers/SearchController.rb
@@ -1,8 +1,27 @@
 class SearchController  < ApplicationController
 
   get ('/') do
-    
+    session[:search] = false
     erb  :search_page
+  end
+
+  get('/1') do
+    search_term = params[:input]
+    session[:search_term] = search_term
+    session[:search] = true
+
+    uri = URI("https://www.themealdb.com/api/json/v1/1/search.php?s=#{search_term}")
+    it = Net::HTTP.get(uri)
+    parsed_it = JSON.parse it 
+    
+    meals = parsed_it
+
+    puts meals[:meals]
+    puts "---meals^^^^^^^^^^^^^^"
+
+    erb :search_page
+
+
   end
 
 end
